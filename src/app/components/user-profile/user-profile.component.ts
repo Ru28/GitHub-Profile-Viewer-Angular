5cb9fc803dccedd21035e0063f226bff488102c4
Rupesh Virani <rupeshvirani01@gmail.com>
2024-04-03 17:48:36 +0530

    made userprofile component and search userDetails components

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { GithubService } from '../../services/github.service';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import  {MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatButtonModule,CommonModule,FormsModule,MatInputModule,MatFormFieldModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
+  username!:string;
+  user!:any;
+  constructor(private githubService: GithubService) { }
 
+  getUserInfo(){
+    console.log("hello");
+    this.githubService.getUser(this.username).subscribe(userData =>{
+      this.user = userData;
+      console.log(userData);
+    })
+  }
 }
